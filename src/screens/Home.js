f2b5261b6f1238577a5b08a84e19cec3ec27740c
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2018-11-04 15:12:07 +0100

    Add loading for home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react'
 import {Constants, Notifications} from 'expo'
 
-import {Alert, Platform, StyleSheet, StatusBar} from 'react-native'
+import {
+  ActivityIndicator,
+  Alert,
+  Platform,
+  StyleSheet,
+  StatusBar,
+  View
+} from 'react-native'
 import {Container} from 'native-base'
 import {withInAppNotification} from '../../lib/react-native-in-app-notification'
 import {Grid, Row} from 'react-native-easy-grid'
@@ -17,6 +24,10 @@ import api from '../services/api'
 import {withAppState} from '../components/appStateProvider'
 
 class Home extends Component {
+  state = {
+    isLoading: true
+  }
+
   componentDidMount() {
     this.fetchTables()
     this.setupListeners()
@@ -76,6 +87,7 @@ class Home extends Component {
     try {
       const res = await api.get(`/api/tables`)
       this.props.actions.setTables(res.data)
+      this.setState({isLoading: false})
     } catch (e) {
       console.error(e)
     }
@@ -96,10 +108,16 @@ class Home extends Component {
             <CreateTableSection navigation={navigation} />
           </Row>
           <Row Row size={80}>
-            <TableList
-              onPress={id => navigation.push('GameTable', {tableId: id})}
-              data={tables}
-            />
+            {this.state.isLoading ? (
+              <View style={styles.loadingContainer}>
+                <ActivityIndicator size="small" color="#c8b273" />
+              </View>
+            ) : (
+              <TableList
+                onPress={id => navigation.push('GameTable', {tableId: id})}
+                data={tables}
+              />
+            )}
           </Row>
         </Grid>
       </Container>
@@ -116,5 +134,10 @@ export default compose(
 const styles = StyleSheet.create({
   screenStyle: {
     backgroundColor: 'black'
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
