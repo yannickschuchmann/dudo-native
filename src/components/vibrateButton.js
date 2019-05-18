fc7797338aed903593835145d6cd77e8aa48411b
Sergio <serg337@gmail.com>
2019-05-18 12:41:14 +0200

    included VibrateButtonLongPress as a class

--- src/components/vibrateButton.js
+++ src/components/vibrateButton.js
@@ -24,4 +24,25 @@ class VibrateButton extends React.Component {
   }
 }
 
+class VibrateButtonLongPress extends React.Component {
+  render() {
+    const props = {
+      ...this.props,
+      onLongPress: e => {
+        //HAPTIC FEEDBACK FOR IOS AND SHORT VIBRATION FOR ANDROID
+        if (ExponentHaptic.notification) {
+          ExponentHaptic.impact(Haptic.ImpactFeedbackStyle.Light)
+        }
+        if (Platform.android) {
+          Vibration.vibrate([0, 25])
+        }
+        //END OF HAPTIC FEEDBACK CODE
+        this.props.onLongPress(e)
+      }
+    }
+    return <Button {...props} />
+  }
+}
+export {VibrateButtonLongPress}
+
 export default VibrateButton
